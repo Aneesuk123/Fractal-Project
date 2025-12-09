29e0265a78ba0c133f51ded6f8f0feb7ff6003c1
anees <anees@gmail.com>
2025-12-10 00:13:54 +0530

    scan

---
+++ scan.ps1
@@ -0,0 +1,14 @@
+# Scan backend
+Write-Host "Scanning backend..."
+cd backend
+trivy fs --ignore-unfixed --severity CRITICAL,HIGH,MEDIUM .
+cd ..
+
+# Scan frontend
+Write-Host "Scanning frontend..."
+cd frontend
+trivy fs --ignore-unfixed --severity CRITICAL,HIGH,MEDIUM .
+cd ..
+
+# Always exit 0
+exit 0
